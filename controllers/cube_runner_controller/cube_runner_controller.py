b3595cc03452e788ca815585db06aa6d414b54ff
Martin Kunz <martin.kunz@student.hu-berlin.de>
2022-06-29 14:18:26 +0200

    write code in main()

--- controllers/cube_runner_controller/cube_runner_controller.py
+++ controllers/cube_runner_controller/cube_runner_controller.py
@@ -2,23 +2,27 @@
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
 
-# create the Robot instance.
-robot = Robot()
+def main():
+    # create the Robot instance.
+    robot = Robot()
+    
+    # get the time step of the current world.
+    timestep = int(robot.getBasicTimeStep())
+    
+    left_motor = robot.getDevice('left_motor')
+    right_motor = robot.getDevice('right_motor')
+    
+    # Initialize motors in order to use velocity control instead of position control.
+    left_motor.setPosition(float('+inf'))
+    right_motor.setPosition(float('+inf'))
+    
+    # Set initial motors velocity.
+    left_motor.setVelocity(1)
+    right_motor.setVelocity(0.8)
+    
+    # Main loop:
+    while robot.step(timestep) != -1:
+        pass
 
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
-
-left_motor = robot.getDevice('left_motor')
-right_motor = robot.getDevice('right_motor')
-
-# Initialize motors in order to use velocity control instead of position control.
-left_motor.setPosition(float('+inf'))
-right_motor.setPosition(float('+inf'))
-
-# Set initial motors velocity.
-left_motor.setVelocity(1)
-right_motor.setVelocity(0.8)
-
-# Main loop:
-while robot.step(timestep) != -1:
-    pass
+if __name__ == '__main__':
+    main()
