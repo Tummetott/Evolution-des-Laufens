7543756cb2b40601a9245b6a5bc748ecae7bfc9b
Tummetott <19708025+Tummetott@users.noreply.github.com>
2022-06-29 17:42:28 +0200

    update

--- controllers/my_controller/my_controller.py
+++ controllers/my_controller/my_controller.py
@@ -1,21 +1,59 @@
+import numpy as np
+import math
+
 from controller import Robot
 
-robot = Robot()
+# L2 (Euclidean) distance
+def distance(a, b):
+    return np.linalg.norm(a - b)
 
+robot = Robot()
 timestep = int(robot.getBasicTimeStep())
 
 legs = []
-
-legnames = ["left","right"]
+legNames = ['left','right']
+angles = []
+angleNames = ['ps_left', 'ps_right']
 
 for i in range(2):
-    legs.append(robot.getDevice(legnames[i]))
+    legs.append(robot.getDevice(legNames[i]))
     legs[i].setPosition(float('inf'))
     legs[i].setVelocity(0.0)
+    angles.append(robot.getDevice(angleNames[i]))
+    angles[i].enable(timestep)
 
+def readAngles():
+    angle_values = [0, 0]
+    angle_values[0] = angles[0].getValue() % math.pi
+    angle_values[1] = angles[1].getValue() % math.pi
+    return angle_values
 
+gps = robot.getDevice('gps')
+gps.enable(timestep)
+
+# Don't know why, but we need a step before we can read the startPosition
+robot.step(timestep)
+startPosition = np.array(gps.getValues())
+startTime = robot.getTime()
+
+# First value is the velocity of the left joint
+# Second value is the velocity of the right joint
+genomVec = [ -1, 1 ]
+
+def elapsedTime():
+    return robot.getTime() - startTime
+
+# Our fitness function only calculates the L2 euclidean distance of the robot
+# from the start position to the current position
+def fitness(genomVec, angleValues, elapsedTime):
+    position = np.array(gps.getValues())
+    return distance(startPosition, position)
+
+# event loop
 while robot.step(timestep) != -1:
-    leftSpeed = -1
-    rightSpeed = 1
-    legs[0].setVelocity(leftSpeed)
-    legs[1].setVelocity(rightSpeed)
+    legs[0].setVelocity(genomVec[0])
+    legs[1].setVelocity(genomVec[1])
+
+    position = np.array(gps.getValues())
+    print(f'distance: {fitness(genomVec, readAngles(), elapsedTime)}')
+    # print(f'angles: {readAngles()}')
